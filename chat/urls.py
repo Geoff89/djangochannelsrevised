4ef11d61f8476c3eef0d0bcbb858bc51f042f99f
Geoffrey Sagini <saginigeoffrey@gmail.com>
2023-01-11 14:23:25 +0300

    add djangochannels example

---
+++ chat/urls.py
@@ -0,0 +1,11 @@
+# chats/urls.py
+
+from django.urls import path
+
+
+from . import views
+
+urlpatterns = [
+    path('', views.index_view, name='chat-index'),
+    path('<str:room_name>/', views.room_view, name='chat-room'),
+]
